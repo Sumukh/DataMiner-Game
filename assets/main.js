b9a817a5bf820b216aef4235ac1a753494846882
Sumukh Sridhara <sumukh@sumukhsridhara.com>
2013-05-08 02:58:41 -0700

    draggable and reverted to old colors

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,6 @@
 var lockLocations = [[100, 600], [120, 150], [200, 170], [136, 270], [311, 312], [229, 358], [621, 412], [808, 307], [480, 376], [415, 486], [274, 497]];
+//Don't hardcode this. 
+//Also imageLocations is bad too.
 
 var imageLocations = [
   'https://mw2.google.com/mw-panoramio/photos/small/6494864.jpg',
@@ -43,10 +45,11 @@ var curBandwidth = 5;
 var curPoints = 100;
 
 var lockString = "";
+/*
 for (var i = 0; i < lockLocations.length; i++) {
   lockString = lockString + '<div class="circle lock" style="left: ' + lockLocations[i][0] + 'px; top: ' + lockLocations[i][1] + 'px;"></div>';
 }
-
+*/
 
 
 function decrementBandwidth() {
